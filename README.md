394cc4024c7e653f8c23efefd2fc4368c15aa092
weifuchunyang <1306701599@qq.com>
2023-10-18 23:28:21 +0800

    初始commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TxProject
+
+Developed with Unreal Engine 5
---
+++ Source/TxProject/TxProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TxProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TXPROJECT_API ATxProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
