cfa0b3b33257a7fb1bff82921296f54df56c01bf
weifuchunyang <1306701599@qq.com>
2023-11-15 17:26:49 +0800

    Shoot enemy

---
+++ Source/TxProject/Private/BTTask_GetPosition.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_GetPosition.h"
+#include "AIController_DeathMatch.h"
+#include "FPSCharacterBase.h"
+#include "NavigationSystem/Public/NavigationSystem.h"
+#include "AIModule/Classes/BehaviorTree/BlackboardComponent.h"
+#include "AIModule/Classes/BehaviorTree/Blackboard/BlackboardKeyType_Vector.h"
+
+EBTNodeResult::Type UBTTask_GetPosition::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController_DeathMatch* ALC = Cast<AAIController_DeathMatch>(OwnerComp.GetOwner());
+	if (ALC)
+	{
+		APawn* AIAttacker = ALC->GetPawn();
+		AFPSCharacterBase* Enemy = ALC->GetEnemy();
+		if (AIAttacker && Enemy)
+		{
+			const FVector Origin = Enemy->GetActorLocation();
+			const float Radius = 800.f;
+
+			UNavigationSystemV1* NavMesh = FNavigationSystem::GetCurrent<UNavigationSystemV1>(ALC);
+			if (NavMesh)
+			{
+				FNavLocation Loc;
+				NavMesh->GetRandomReachablePointInRadius(Origin, Radius, Loc);
+				if (Loc.Location != FVector::ZeroVector)
+				{
+					OwnerComp.GetBlackboardComponent()->SetValue<UBlackboardKeyType_Vector>(BlackboardKey.GetSelectedKeyID(), Loc.Location);
+					return EBTNodeResult::Succeeded;
+				}
+			}
+		}
+	}
+
+	return EBTNodeResult::Failed;
+}
